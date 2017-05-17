6c5a15a0ff8ec146018f4aa763ebf9234327555c
Jose Maria Rincon <jomari94@gmail.com>
2017-05-17 23:56:57 +0200

    Habilitando CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+io.set('origins', '*:*');
 
 http.listen(process.env.PORT || 3000, function(){
     console.log('listening');
