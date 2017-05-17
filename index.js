d1ba7a17c7e6ff6e55bf8b5824ca67b79acb8c19
Jose Maria Rincon <jomari94@gmail.com>
2017-05-17 23:48:02 +0200

    carga inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(process.env.PORT || 3000, function(){
+    console.log('listening');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+        socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
