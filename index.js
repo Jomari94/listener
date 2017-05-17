1092e03010ece822e49fd71802a6c6070b87e86e
Jose Maria Rincon <jomari94@gmail.com>
2017-05-18 00:05:31 +0200

    Habilitando CORS

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 io.set('origins', '*:*');
 
 http.listen(process.env.PORT || 3000, function(){
