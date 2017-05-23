bd0bcd83cc9aba1bde4ad97a6e686f4a1fcd7144
Jose Maria Rincon <jomari94@gmail.com>
2017-05-23 19:11:07 +0200

    Ahora soporta salas

--- index.js
+++ index.js
@@ -19,17 +19,37 @@ app.use(allowCrossDomain);
 
 io.set('origins', '*:*');
 
+var people = {};
+
 http.listen(process.env.PORT || 3000, function(){
     console.log('listening');
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-        socket.on('disconnect', function(){
-        console.log('user disconnected');
+
+    socket.on("join", function(name){
+        people[socket.id] = name;
+        console.log(people[socket.id] + ' connected');
     });
+
+    socket.on('switchRoom', function(newroom){
+        // leave the current room (stored in session)
+        socket.leave(socket.room);
+        // join new room, received as function parameter
+        socket.join(newroom);
+        socket.room = newroom;
+        console.log(socket.room);
+        io.in(socket.room).emit('joined', people[socket.id]);
+    });
+
+    socket.on('disconnect', function(){
+        console.log(people[socket.id] + ' disconnected');
+        io.in(socket.room).emit('leave', people[socket.id]);
+        delete people[socket.id];
+    });
+
     socket.on('chat message', function(msg){
       console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        io.in(socket.room).emit('chat message', msg);
     });
 });
